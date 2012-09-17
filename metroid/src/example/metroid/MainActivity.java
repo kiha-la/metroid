b6b8132093b1e4629efe18c926b70b983b860a6c
kaela_000 <kaela_000@Windows8>
2012-09-17 23:13:36 +0900

    セクションごとに異なるレイアウトを作成

--- metroid/src/example/metroid/MainActivity.java
+++ metroid/src/example/metroid/MainActivity.java
@@ -20,95 +20,135 @@ import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide fragments for each of the
-     * sections. We use a {@link android.support.v4.app.FragmentPagerAdapter} derivative, which will
-     * keep every loaded fragment in memory. If this becomes too memory intensive, it may be best
-     * to switch to a {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
+	/**
+	 * The {@link android.support.v4.view.PagerAdapter} that will provide
+	 * fragments for each of the sections. We use a
+	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
+	 * will keep every loaded fragment in memory. If this becomes too memory
+	 * intensive, it may be best to switch to a
+	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+	 */
+	SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    ViewPager mViewPager;
+	/**
+	 * The {@link ViewPager} that will host the section contents.
+	 */
+	ViewPager mViewPager;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // Create the adapter that will return a fragment for each of the three primary sections
-        // of the app.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// Create the adapter that will return a fragment for each of the three
+		// primary sections
+		// of the app.
+		mSectionsPagerAdapter = new SectionsPagerAdapter(
+				getSupportFragmentManager());
 
+		// Set up the ViewPager with the sections adapter.
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+	}
 
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	/**
+	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+	 * one of the primary sections of the app.
+	 */
+	public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-    
+		public SectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
 
+		/*
+		 * //引数iで指定されたページに表示するFragmentインスタントを返す. public Fragment getItem(int i)
+		 * { Fragment fragment = new MainActivityFragment(); Bundle args = new
+		 * Bundle(); args.putInt(MainActivityFragment.ARG_SECTION_NUMBER, i +
+		 * 1); fragment.setArguments(args); return fragment; }
+		 */
+		@Override
+		// 引数iで指定されたページに表示するFragmentインスタントを返す.
+		
+		/*
+		public Fragment getItem(int i) {
+			Fragment fragment1 = new MainActivityFragment1();
+			Fragment fragment2 = new MainActivityFragment2();
+			Fragment fragment3 = new MainActivityFragment3();
+			Bundle args = new Bundle();
+			args.putInt(MainActivityFragment1.ARG_SECTION_NUMBER, i + 1);
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to one of the primary
-     * sections of the app.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+			Fragment fragment = null;
+			
+			
+			fragment.setArguments(args);
+			 
+			switch (i) {
+			case 1:
+				return fragment=fragment1; 
+			case 2:
+				return fragment=fragment2; 
+			case 3:
+				return fragment=fragment3; 
+			}
+			
+			return fragment;
+		
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
+		}
+		*/
+		
+		public Fragment getItem(int i) {
+			Fragment fragment1 = new MainActivityFragment1();
+			Fragment fragment2 = new MainActivityFragment2();
+			Fragment fragment3 = new MainActivityFragment3();
+			//Bundle args = new Bundle();
+			//args.putInt(MainActivityFragment1.ARG_SECTION_NUMBER, i + 1);
 
-        @Override
-        public Fragment getItem(int i) {
-            Fragment fragment = new DummySectionFragment();
-            Bundle args = new Bundle();
-            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, i + 1);
-            fragment.setArguments(args);
-            return fragment;
-        }
+			Fragment fragment = null;
+			
+			
+			//fragment.setArguments(args);
+			 
+			switch (i) {
+			case 0:
+				return fragment=fragment1; 
+			case 1:
+				return fragment=fragment2; 
+			case 2:
+				return fragment=fragment3; 
+			}
+			
+			
+			return fragment1;
+		
 
-        @Override
-        public int getCount() {
-            return 3;
-        }
+		}
 
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0: return getString(R.string.title_section1).toUpperCase();
-                case 1: return getString(R.string.title_section2).toUpperCase();
-                case 2: return getString(R.string.title_section3).toUpperCase();
-            }
-            return null;
-        }
-    }
+		@Override
+		// ページ数を返す
+		public int getCount() {
+			return 3;
+		}
 
-    /**
-     * A dummy fragment representing a section of the app, but that simply displays dummy text.
-     */
-    public static class DummySectionFragment extends Fragment {
-        public DummySectionFragment() {
-        }
+		@Override
+		public CharSequence getPageTitle(int position) {
+			switch (position) {
+			case 0:
+				return getString(R.string.title_section1).toUpperCase();
+			case 1:
+				return getString(R.string.title_section2).toUpperCase();
+			case 2:
+				return getString(R.string.title_section3).toUpperCase();
+			}
+			return null;
+		}
+	}
 
-        public static final String ARG_SECTION_NUMBER = "section_number";
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            TextView textView = new TextView(getActivity());
-            textView.setGravity(Gravity.CENTER);
-            Bundle args = getArguments();
-            textView.setText(Integer.toString(args.getInt(ARG_SECTION_NUMBER)));
-            return textView;
-        }
-    }
 }
---
+++ metroid/src/example/metroid/MainActivityFragment3.java
@@ -0,0 +1,27 @@
+package example.metroid;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A dummy fragment representing a section of the app, but that simply displays dummy text.
+ */
+public class MainActivityFragment3 extends Fragment {
+    public MainActivityFragment3() {
+    }
+
+    public static final String ARG_SECTION_NUMBER = "section_number";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+    	
+    	 return inflater.inflate(R.layout.section3, container, false);
+    }
+}
